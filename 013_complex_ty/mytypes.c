fc5423e3d16f55b5f917dcc7907e19912adcc46b
qg45 <qg45@duke.edu>
2021-08-29 22:23:25 -0400

    013 done-2

--- 013_complex_ty/mytypes.c
+++ 013_complex_ty/mytypes.c
@@ -5,4 +5,4 @@ struct _complex_num_t {
 typedef struct _complex_num_t complex_num_t;
 
 enum _animal_t { CAT, FISH, SNAKE, DOG, HORSE };
-typedef enum _animal_t enum_animal_t;
+typedef enum _animal_t animal_t;
