5e9f9ded112d19b6c3259332e1424503d8167467
qg45 <qg45@duke.edu>
2021-09-05 08:39:18 -0400

    025 done-1

--- 025_debugging/game.c
+++ 025_debugging/game.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-int getSecretNumber(void); //prototype, implemented elsewhere.
+int getSecretNumber(void);  //prototype, implemented elsewhere.
 
-int getOtherSN(int which); //prototype, implemented elsewhere.
+int getOtherSN(int which);  //prototype, implemented elsewhere.
 
 int main(void) {
   int guessesMade = 0;
@@ -13,24 +13,24 @@ int main(void) {
 
   printf("I'm thinking of a number...\n");
   printf("What number do you guess?\n");
-  if(fgets(buffer, 1024, stdin) == NULL) {
+  if (fgets(buffer, 1024, stdin) == NULL) {
     printf("Oh no, you are giving up?  You lose...\n");
     return EXIT_FAILURE;
   }
   yourGuess = atoi(buffer);
-  if(yourGuess != myNumber) {
+  if (yourGuess != myNumber) {
     printf("I'm sorry, that is not right.  You lose\n");
     return EXIT_FAILURE;
   }
   printf("Correct! You win round1!\n");
-  
+
   int total = 0;
   for (int i = 0; i <= 5678; i++) {
     total = total ^ getOtherSN(i);
   }
   printf("Ok, time for round 2. I have another secret number.\n");
   printf("Your guess:\n");
-  if(fgets(buffer, 1024, stdin) == NULL) {
+  if (fgets(buffer, 1024, stdin) == NULL) {
     printf("Oh no, you are giving up?  You lose...\n");
     return EXIT_FAILURE;
   }
