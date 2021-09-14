34f9012eb4d44ad2bf90dfa371dc8e95b923572f
qg45 <qg45@duke.edu>
2021-09-13 21:22:58 -0400

    036 done-2

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -3,7 +3,7 @@
 
 size_t maxSeq(int * array, size_t n);
 
-void run_check(int * array, size_t n, size_t expected_ans) {
+void check(int * array, size_t n, size_t expected_ans) {
   if (maxSeq(array, n) != expected_ans) {
     printf("A broken case found\n");
     exit(EXIT_FAILURE);
@@ -11,12 +11,29 @@ void run_check(int * array, size_t n, size_t expected_ans) {
 }
 
 int main(void) {
-  int a1[7] = {1, 1, 2, 4, 5, 7, 9};
-  run_check(a1, 7, 6);
-  int a2[1] = {9};
-  run_check(a2, 1, 1);
-  int a3[5] = {-1, 0, 1, 2, 3};
-  run_check(a3, 5, 5);
-  int a4[5] = {0};
-  run_check(a4, 5, 0);
+  int array1[10] = {1, 2, 1, 3, 5, 8, 2, 4, 6, 9};
+  int array2[7] = {-1, 0, 1, 2, 4, 3, 5};
+  int array3[7] = {1, 1, 1, 3, 5, 8, 1};
+  int array4[7] = {-4, -3, -2, -1, 0, -5, -6};
+  int array5[5] = {1, 1, 1, 1, 1};
+  int array6[6] = {0};
+  int array7[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+  int array8[3] = {3, 2, 1};
+  int array9[1] = {2};
+  int array10[6] = {-1, -2, -3, 0, 3, 8};
+  int array11[12] = {1, 2, 3, 1, 2, 3, 4, 5, 1, 2, 3, 4};
+  int * arr12 = NULL;
+  check(array1, 10, 4);
+  check(array2, 7, 5);
+  check(array3, 7, 4);
+  check(array4, 7, 5);
+  check(array5, 5, 1);
+  check(array6, 6, 1);
+  check(array7, 8, 8);
+  check(array8, 8, 1);
+  check(array9, 1, 1);
+  check(array10, 6, 4);
+  check(array11, 12, 5);
+  check(arr12, 0, 0);
+  return EXIT_SUCCESS;
 }
