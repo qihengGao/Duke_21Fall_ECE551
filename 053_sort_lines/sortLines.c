bbd0c3e3f312a55ceabfe3611c07e2f448fd0050
qg45 <qg45@duke.edu>
2021-09-26 18:11:54 -0400

    053 done

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,46 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void sortLines(FILE * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t size;
+  size_t count = 0;
+  while (getline(&curr, &size, f) != -1) {
+    lines = realloc(lines, (count + 1) * sizeof(*lines));
+    lines[count] = curr;
+    count++;
+  }
+  free(curr);
+  sortData(lines, count);
+  for (size_t i = 0; i < count; i++) {
+    printf("%s", lines[i]);
+  }
+  free(lines);
+}
+
+void sortLinesInFiles(int argc, char ** argv) {
+  for (int i = 0; i < argc - 1; i++) {
+    FILE * f = fopen(argv[i + 1], "r");
+    if (!f) {
+      fprintf(stderr, "Cannot open file.\n");
+      exit(EXIT_FAILURE);
+    }
+    sortLines(f);
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Cannot close the file.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    sortLines(stdin);
+  }
+  else if (argc > 1) {
+    sortLinesInFiles(argc, argv);
+  }
   return EXIT_SUCCESS;
 }
