a590e3da9374db731f6b0a3fa3641a5464ba52cb
qg45 <qg45@duke.edu>
2021-09-25 22:33:31 -0400

    add documentations to step1

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Function prototypes. Please see the detailed descriptions and implementations at the bottom. */
 void parseName(const char * line, const char * comma, country_t * country_p);
 void parsePopulation(const char * comma, country_t * country_p);
 
@@ -16,12 +17,12 @@ country_t parseLine(char * line) {
   country.name[0] = '\0';
   country.population = 0;
 
-  /** Sanity check: input line has contents.*/
+  /* Sanity check: input line has contents.*/
   if (line == NULL || strlen(line) == 0) {
     fprintf(stderr, "Empty Input.\n");
     exit(EXIT_FAILURE);
   }
-
+  /* Check if the comma exists. */
   const char * comma = strchr(line, ',');
   if (comma == NULL) {
     fprintf(
@@ -29,8 +30,9 @@ country_t parseLine(char * line) {
         "The input line does not match the standard format: a comma does not exist.\n");
     exit(EXIT_FAILURE);
   }
-
+  /*  We consider characters before the first comma as the name of the country. */
   parseName(line, comma, &country);
+  /* The digits after the first comma as the population. */
   parsePopulation(comma, &country);
 
   return country;
@@ -51,31 +53,46 @@ void printCountryWithMax(country_t * countries,
   //WRITE ME
 }
 
+/* Parse the name in line, print out error messages and exit if an error occurs. */
 void parseName(const char * line, const char * comma, country_t * country_p) {
   const char * name_start;
-  const char * name_end;
   name_start = line;
-  name_end = line;
-  while (name_end != comma) {
-    name_end++;
-  }
-  size_t name_len = (size_t)(name_end - name_start);
+
+  /* We can compute the country name length by taking the difference of pointer comma and pointer name_start.
+   * As the name_start stores the address of the first character and comma stores the address of the comma character,
+   * their difference is the number of characters from the name_start (inclusive) to the comma (exclusive).
+   */
+  size_t name_len = (size_t)(comma - name_start);
+
+  /* Check if the name length is too long */
   if (name_len > MAX_NAME_LEN - 1) {
-    fprintf(stderr, "The name is too long.\n");
+    fprintf(stderr, "The name is too long. (length <= 63)\n");
     exit(EXIT_FAILURE);
   }
+
+  /* Copy the characters to the country_t's name field. */
   memcpy(country_p->name, name_start, name_len);
+  /* Set terminator properly. */
   country_p->name[name_len] = '\0';
 }
 
+/* Parse the population in line, print out  print out error messages and exit if an error occurs. */
 void parsePopulation(const char * comma, country_t * country_p) {
+  /* Initialize two pointers which both point at the one character after the comma. */
   const char * population_start;
   const char * population_end;
   population_start = comma + 1;
   population_end = comma + 1;
+
+  /* Iterate on the rest of line to check if it only contains digits. */
+  /* Set the end character  where the iteration should end, generally it is '\n', but if it is not exist,
+   * then the line ends without a '\n', so we set the end character to '\0'. 
+   */
   const char * end_p = strchr(population_start, '\n');
   char end = (end_p == NULL) ? '\0' : *end_p;
+  /* Iteration starts. */
   while (*population_end != end) {
+    /* Non-digit character occurs, exit. */
     if (!isdigit(*population_end)) {
       fprintf(stderr,
               "Unexpected character: '%c' appears in the population information, which "
@@ -85,15 +102,22 @@ void parsePopulation(const char * comma, country_t * country_p) {
     }
     population_end++;
   }
+
+  /* Similar computation as getting the name length in parseName. */
   size_t population_len = (size_t)(population_end - population_start);
   if (population_len == 0) {
     fprintf(stderr, "Population information is not included in input.\n");
     exit(EXIT_FAILURE);
   }
+
+  /* Parse string to integer. As the country.population is uint64_t, use strtoul. */
   unsigned long int population = strtoul(population_start, NULL, 10);
+  /* Check overflow. */
   if (population == ULONG_MAX && errno == ERANGE) {
     fprintf(stderr, "Input population is out of range.\n");
     exit(EXIT_FAILURE);
   }
+
+  /* Assign the parsed integer to coutry.population. */
   country_p->population = (uint64_t)population;
 }
