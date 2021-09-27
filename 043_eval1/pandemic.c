d39b5481c6adb89432182e03fa4581487281363f
qg45 <qg45@duke.edu>
2021-09-26 22:33:03 -0400

    eval1 done

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -8,9 +8,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Function prototypes. Please see the detailed descriptions and implementations at the bottom. */
+/* Function prototypes.
+ * Please see the detailed descriptions and implementations at the bottom. */
 void parseName(const char * line, const char * comma, country_t * country_p);
 void parsePopulation(const char * comma, country_t * country_p);
+unsigned getMaxCaseNum(unsigned * case_array, size_t size);
 
 country_t parseLine(char * line) {
   country_t country;
@@ -39,18 +41,121 @@ country_t parseLine(char * line) {
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
-  //WRITE ME
+  /** NULL check. */
+  if (data == NULL) {
+    fprintf(stderr, "Input data is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+  if (avg == NULL) {
+    fprintf(stderr, "Input avg for recording is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /** Check if the input is sufficient. */
+  if (n_days < 7) {
+    return;
+  }
+
+  /* Initialize an index indicator which indicates 
+   * the next index of avg where the result should be stored*/
+  size_t next_to_fill = 0;
+
+  /* We carry out 'sliding window' approach, 
+   * as we believe this approach can circumvent duplicated computation.  */
+  /* The 'window' on the data array is characterized by the 'start' and 'end', 
+   * and the window size is fixed as 7. 
+   * curr_sum keeps track of  the sum of the elements in window.
+   * when end = 6 (0-based), this is the first time our 'window' gets the size 7.
+   * Then we can get the result by curr_sum / 7.0, put the result in the proper index.
+   * increase next_to_fill by 1.
+   * To keep the size fixed at 7, we 'squeeze the window' through increasing window start by 1,
+   * 'making space' for window to include newly incoming elements.
+   * Iterate through the data array unitl our window hits the end. */
+  size_t curr_sum = 0;
+  for (size_t start = 0, end = 0; end < n_days; end++) {
+    curr_sum += data[end];
+    if (end >= 6) {
+      avg[next_to_fill] = curr_sum / 7.0;
+      next_to_fill++;
+      curr_sum -= data[start];
+      start++;
+    }
+  }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
-  //WRITE ME
+  /* NULL check. */
+  if (data == NULL) {
+    fprintf(stderr, "The input data is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (cum == NULL) {
+    fprintf(stderr, "Input cum for recoding is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keep track of cummulative amout of cases. */
+  size_t cumulative_sum = 0;
+
+  /* Initialize an index indicator which indicates 
+   * the next index of cum  where the result should be stored*/
+  size_t next_to_fill = 0;
+
+  /* Iterate on daily cases, calculating cumulative cases per 100,000 people. */
+  for (size_t i = 0; i < n_days; i++) {
+    cumulative_sum += data[i];
+    cum[next_to_fill] = (cumulative_sum / (double)pop) * 100000;
+    next_to_fill++;
+  }
 }
 
 void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
-  //WRITE ME
+  /* NULL check. */
+  if (countries == NULL) {
+    fprintf(stderr, "The input information of countries is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (data == NULL || *data == NULL) {
+    fprintf(stderr, "The input cases data is not exist.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Initialize the maximum as -1 for later compaison. */
+  unsigned max_case_num = 0;
+  /* The coresponding country which has the maximum daily cases. */
+  char * country_name = NULL;
+  /* Keep track of the frequency of maximum in order to detect tie. */
+  size_t frequency_of_max = 0;
+
+  /* Iterate through the 2D array. */
+  /* Obtain maximum cases for each country.
+   * Update the corresponding country_name and max_case_num only when meet a greater case number,
+   * and restore the frequency count to 1.
+   * Or when meet a case number that is equal to current max_case_num, increase the frequency
+   * to indicate a potential tie. */
+  for (size_t i = 0; i < n_countries; i++) {
+    unsigned max_cases_county_i = getMaxCaseNum(data[i], n_days);
+    if (max_cases_county_i > max_case_num) {
+      max_case_num = max_cases_county_i;
+      country_name = countries[i].name;
+      frequency_of_max = 1;
+    }
+    else if (max_cases_county_i == max_case_num) {
+      frequency_of_max++;
+    }
+  }
+
+  if (frequency_of_max > 1) { /* There exits a tie as the maximum is not unique. */
+    printf("There is a tie between at least two countries\n");
+  }
+  else {
+    printf("%s has the most daily cases with %u\n", country_name, max_case_num);
+  }
 }
 
 /* Parse the name in line, print out error messages and exit if an error occurs. */
@@ -58,9 +163,12 @@ void parseName(const char * line, const char * comma, country_t * country_p) {
   const char * name_start;
   name_start = line;
 
-  /* We can compute the country name length by taking the difference of pointer comma and pointer name_start.
-   * As the name_start stores the address of the first character and comma stores the address of the comma character,
-   * their difference is the number of characters from the name_start (inclusive) to the comma (exclusive).
+  /* We can compute the country name length 
+   * by taking the difference of pointer comma and pointer name_start.
+   * As the name_start stores the address of the first character
+   * and comma stores the address of the comma character,
+   * their difference is the number of characters
+   * from the name_start (inclusive) to the comma (exclusive).
    */
   size_t name_len = (size_t)(comma - name_start);
 
@@ -86,7 +194,8 @@ void parsePopulation(const char * comma, country_t * country_p) {
 
   /* We would argue that the population part is considered valid iff it contains digits only. */
   /* Iterate on the rest of line to check if it only contains digits. */
-  /* Set the end character where the iteration should end, generally it is '\n', but if it is not exist,
+  /* Set the end character where the iteration should end, 
+   * generally it is '\n', but if it is not exist,
    * then the line ends without a '\n', so we set the end character to '\0'. */
   const char * end_p = strchr(population_start, '\n');
   char end = (end_p == NULL) ? '\0' : *end_p;
@@ -120,4 +229,21 @@ void parsePopulation(const char * comma, country_t * country_p) {
 
   /* Assign the parsed integer to coutry.population. */
   country_p->population = (uint64_t)population;
+}
+
+/* Return the maximum value of the given array of cases. */
+unsigned getMaxCaseNum(unsigned * case_array, size_t size) {
+  if (case_array == NULL) {
+    fprintf(stderr, "Input case array is empty.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  unsigned maximum = 0;
+  for (size_t i = 0; i < size; i++) {
+    if (case_array[i] > maximum) {
+      maximum = case_array[i];
+    }
+  }
+
+  return maximum;
 }
