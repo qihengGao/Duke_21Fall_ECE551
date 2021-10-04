c41b433e6d09c56520509c3796c816bb770c799b
qg45 <qg45@duke.edu>
2021-10-04 15:06:10 -0400

    057_done

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,14 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
+#define SUFFIX_LEN 8  // .counts\0
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  size_t name_len = strlen(inputName);
+  char * output_name = malloc((name_len + SUFFIX_LEN) * sizeof(*output_name));
+  memcpy(output_name, inputName, name_len);
+  memcpy(output_name + name_len, ".counts", SUFFIX_LEN);
+  return output_name;
 }
