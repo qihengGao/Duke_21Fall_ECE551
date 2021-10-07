c6bd836d99e632d3f30f332b4f1f8f9b96c0ed40
qg45 <qg45@duke.edu>
2021-10-06 22:45:50 -0400

    058_done

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,73 @@
+#include "counts.h"
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
+
+one_count_t * createOneNode(const char * name, size_t count);
+void addNewNode(counts_t * c, const char * name);
+
 counts_t * createCounts(void) {
-  //WRITE ME
+  counts_t * counts = malloc(sizeof(*counts));
+  counts->nodes = NULL;
+  counts->size = 0;
+  counts->next = 0;
+  counts->unknown = 0;
+  return counts;
 }
 void addCount(counts_t * c, const char * name) {
-  //WRITE ME
+  if (name == NULL) {
+    c->unknown++;
+    return;
+  }
+  for (size_t i = 0; i < c->next; i++) {
+    if (strcmp((c->nodes[i])->val, name) == 0) {
+      (c->nodes[i])->count++;
+      return;
+    }
+  }
+  addNewNode(c, name);
 }
 void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+  if (c->next == 0)
+    return;
+  // zu for size_t
+  for (size_t i = 0; i < c->next; i++) {
+    fprintf(outFile, "%s: %zu\n", c->nodes[i]->val, c->nodes[i]->count);
+  }
+  fprintf(outFile, "%s: %zu\n", "<unknown>", c->unknown);
 }
 
 void freeCounts(counts_t * c) {
-  //WRITE ME
+  for (size_t i = 0; i < c->next; i++) {
+    free(c->nodes[i]->val);
+    free(c->nodes[i]);
+  }
+  free(c->nodes);
+  free(c);
+}
+
+void addNewNode(counts_t * c, const char * name) {
+  if (c->next == c->size) {
+    if (c->size == 0) {
+      c->size = 1;
+    }
+    c->nodes = realloc(c->nodes, 2 * c->size * sizeof(*c->nodes));
+    assert(c->nodes);
+    c->size *= 2;
+  }
+  one_count_t * node = createOneNode(name, 1);
+  assert(c->next < c->size);
+  c->nodes[c->next] = node;
+  c->next++;
+}
+
+one_count_t * createOneNode(const char * name, size_t count) {
+  one_count_t * node = malloc(sizeof(*node));
+  node->val = malloc((strlen(name) + 1) * sizeof(*node->val));
+  node->count = count;
+  memcpy(node->val, name, strlen(name));
+  node->val[strlen(name)] = '\0';
+  return node;
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,19 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
-struct _one_count_t {
-  //DEFINE ME
 
+#include <stdio.h>
+
+struct _one_count_t {
+  char * val;
+  size_t count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
-  //DEFINE ME
+  one_count_t ** nodes;
+  size_t size;
+  size_t next;
+  size_t unknown;
 };
 typedef struct _counts_t counts_t;
 
