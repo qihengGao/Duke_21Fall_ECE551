f3aab560b96f2591d71da13ec3ec0006ece824e6
qg45 <qg45@duke.edu>
2021-10-13 00:21:41 -0400

    070

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -17,12 +17,11 @@ double Circle::intersectionArea(const Circle & c) {
     double r_min = (this->r < c.r) ? this->r : c.r;
     return M_PI * r_min * r_min;
   }
-  double d1 = this->r * this->r - c.r * c.r + d * d / 2 * d;
+  double d1 = (this->r * this->r - c.r * c.r + d * d) / (2 * d);
   double d2 = d - d1;
   double acos_1 = std::acos(d1 / this->r);
   double acos_2 = std::acos(d2 / c.r);
-  double squared_diff_1 = std::sqrt(this->r * this->r - d1 * d1);
-  double squared_diff_2 = std::sqrt(c.r * c.r - d2 * d2);
-  return this->r * this->r * acos_1 - d1 * squared_diff_1 + c.r * c.r * acos_2 -
-         d2 * squared_diff_2;
+  return this->r * this->r * acos_1 + c.r * c.r * acos_2 -
+         0.5 * std::sqrt((-d + this->r + c.r) * (d + this->r - c.r) *
+                         (d - this->r + c.r) * (d + this->r + c.r));
 }
