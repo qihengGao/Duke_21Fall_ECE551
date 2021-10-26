d3a49544dfa4f5145c1fdcdb862ea68839e1c24a
qg45 <qg45@duke.edu>
2021-10-25 20:18:47 -0400

    adjust

--- 083_expr_eval1/expr.hpp
+++ 083_expr_eval1/expr.hpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <string>
 
 class Expression {
@@ -10,8 +11,12 @@ class NumExpression : public Expression {
   long num;
 
  public:
-  NumExpression(long num);
-  virtual std::string toString() const;
+  NumExpression(long num) : num(num){};
+  virtual std::string toString() const {
+    std::stringstream result;
+    result << this->num;
+    return result.str();
+  };
   // virtual ~NumExpression();
 };
 
@@ -21,12 +26,24 @@ class OperatorExpression : public Expression {
   std::string op;
 
  public:
-  OperatorExpression(Expression * lhs, Expression * rhs, std::string op);
-  virtual std::string toString() const;
-  virtual ~OperatorExpression();
+  OperatorExpression(Expression * lhs, Expression * rhs, std::string op) :
+      lhs(lhs),
+      rhs(rhs),
+      op(op){};
+  virtual std::string toString() const {
+    std::stringstream expression;
+    expression << '(' << this->lhs->toString() << ' ' << this->op << ' '
+               << this->rhs->toString() << ')';
+    return expression.str();
+  }
+  virtual ~OperatorExpression() {
+    delete lhs;
+    delete rhs;
+  };
 };
 
 class PlusExpression : public OperatorExpression {
  public:
-  PlusExpression(Expression * lhs, Expression * rhs);
+  PlusExpression(Expression * lhs, Expression * rhs) :
+      OperatorExpression(lhs, rhs, "+"){};
 };
