88c1730d2101cf67f3f90d8126a5d292d4d5d477
qg45 <qg45@duke.edu>
2021-11-02 01:06:37 -0400

    done

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -5,6 +5,9 @@
 
 #include <cstdlib>
 #include <exception>
+class IndexException : public std::exception {
+  virtual const char * what() const throw() { return "Index is out of range."; }
+};
 
 template<typename T>
 class LinkedList {
@@ -19,130 +22,31 @@ class LinkedList {
   Node * head;
   Node * tail;
   int size;
-  class IndexException : public std::exception {
-    const char * what() { return "Index is out of range."; }
-  };
 
  public:
   LinkedList() : head(NULL), tail(NULL), size(0) {}
   LinkedList(const LinkedList<T> & rhs);
   LinkedList<T> & operator=(const LinkedList<T> & rhs);
   ~LinkedList();
-
-  void addFront(const T & val) {
-    head = new Node(NULL, val, this->head);
-    if (this->tail == NULL) {
-      this->tail = this->head;
-    }
-    else {
-      head->next->prev = head;
-    }
-    size++;
-  }
-
-  void addBack(const T & val) {
-    tail = new Node(tail, val, NULL);
-    if (head == NULL) {
-      head = tail;
-    }
-    else {
-      this->tail->prev->next = this->tail;
-    }
-    size++;
-  }
-
-  bool remove(const T & val) {
-    Node ** curr = &(this->head);
-    while (*curr != NULL) {
-      if ((*curr)->data != val) {
-        curr = &((*curr)->next);
-      }
-      else {
-        Node * toDel = *curr;
-        *curr = (*curr)->next;
-        // head or midlle
-        if (toDel->next != NULL) {
-          toDel->next->prev = toDel->prev;
-        }
-        // tail
-        if (toDel->next == NULL) {
-          this->tail = toDel->prev;
-        }
-        delete toDel;
-        this->size--;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  T & operator[](int i) {
-    Node * p = this->head;
-    if (i < 0) {
-      throw IndexException();
-    }
-    while ((p != NULL) && i > 0) {
-      p = p->next;
-      i--;
-    }
-    if (p == NULL) {
-      throw IndexException();
-    }
-    return p->data;
-  }
-
-  const T & operator[](int i) const {
-    Node * p = this->head;
-    if (i < 0) {
-      throw IndexException();
-    }
-    while ((p != NULL) && i > 0) {
-      p = p->next;
-      i--;
-    }
-    if (p == NULL) {
-      throw IndexException();
-    }
-    return p->data;
-  }
-
-  int find(const T & val) {
-    if (head == NULL) {
-      return -1;
-    }
-    int i = 0;
-    Node * curr = this->head;
-    while (curr != NULL) {
-      if (curr->data == val) {
-        return i;
-      }
-      curr = curr->next;
-      i++;
-    }
-    return -1;
-  }
-
-  int getSize() const { return this->size; }
+  void addFront(const T & val);
+  void addBack(const T & val);
+  bool remove(const T & val);
+  T & operator[](int i);
+  const T & operator[](int i) const;
+  int find(const T & val);
+  int getSize() const;
 };
 
 template<typename T>
 LinkedList<T>::LinkedList(const LinkedList<T> & rhs) {
+  head = NULL;
+  tail = NULL;
+  this->size = rhs.size;
   Node * curr = rhs.head;
   while (curr != NULL) {
     addBack(curr->data);
     curr = curr->next;
   }
-}
-
-template<typename T>
-LinkedList<T>::~LinkedList() {
-  while (head != NULL) {
-    Node * next = head->next;
-    delete head;
-    head = next;
-  }
-  head = NULL;
-  tail = NULL;
 }
 
 template<typename T>
@@ -158,5 +62,120 @@ LinkedList<T> & LinkedList<T>::operator=(const LinkedList<T> & rhs) {
     size = temp.size;
   }
   return *this;
+}
+
+template<typename T>
+LinkedList<T>::~LinkedList() {
+  while (head != NULL) {
+    Node * next = head->next;
+    delete head;
+    head = next;
+  }
+  head = NULL;
+  tail = NULL;
+}
+
+template<typename T>
+void LinkedList<T>::addFront(const T & val) {
+  head = new Node(NULL, val, this->head);
+  if (this->tail == NULL) {
+    this->tail = this->head;
+  }
+  else {
+    head->next->prev = head;
+  }
+  size++;
+}
+
+template<typename T>
+void LinkedList<T>::addBack(const T & val) {
+  tail = new Node(tail, val, NULL);
+  if (head == NULL) {
+    head = tail;
+  }
+  else {
+    this->tail->prev->next = this->tail;
+  }
+  size++;
+}
+
+template<typename T>
+bool LinkedList<T>::remove(const T & val) {
+  Node ** curr = &(this->head);
+  while (*curr != NULL) {
+    if ((*curr)->data != val) {
+      curr = &((*curr)->next);
+    }
+    else {
+      Node * toDel = *curr;
+      *curr = (*curr)->next;
+      // head or midlle
+      if (toDel->next != NULL) {
+        toDel->next->prev = toDel->prev;
+      }
+      // tail
+      if (toDel->next == NULL) {
+        this->tail = toDel->prev;
+      }
+      delete toDel;
+      this->size--;
+      return true;
+    }
+  }
+  return false;
+}
+
+template<typename T>
+T & LinkedList<T>::operator[](int i) {
+  Node * p = this->head;
+  if (i < 0) {
+    throw IndexException();
+  }
+  while ((p != NULL) && i > 0) {
+    p = p->next;
+    i--;
+  }
+  if (p == NULL) {
+    throw IndexException();
+  }
+  return p->data;
+}
+
+template<typename T>
+const T & LinkedList<T>::operator[](int i) const {
+  Node * p = this->head;
+  if (i < 0) {
+    throw IndexException();
+  }
+  while ((p != NULL) && i > 0) {
+    p = p->next;
+    i--;
+  }
+  if (p == NULL) {
+    throw IndexException();
+  }
+  return p->data;
+}
+
+template<typename T>
+int LinkedList<T>::find(const T & val) {
+  if (head == NULL) {
+    return -1;
+  }
+  int i = 0;
+  Node * curr = this->head;
+  while (curr != NULL) {
+    if (curr->data == val) {
+      return i;
+    }
+    curr = curr->next;
+    i++;
+  }
+  return -1;
+}
+
+template<typename T>
+int LinkedList<T>::getSize() const {
+  return this->size;
 }
 #endif
