5849fe2c84eb30a4c1689de8ec7ebc6c8ec06df9
qg45 <qg45@duke.edu>
2021-11-14 11:14:43 -0500

    103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,19 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+Node * buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+
+  while (pq.size() > 1) {
+    Node * nodeA = pq.top();
+    pq.pop();
+    Node * nodeB = pq.top();
+    pq.pop();
+    pq.push(new Node(nodeA, nodeB));
+  }
+  return pq.top();
+}
